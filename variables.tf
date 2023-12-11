bb9821d838009b9512fb27ef36c7280e55efdbf0
simcowhist <asaf@whist.co.il>
2023-12-11 14:36:59 +0200

    added ec2 instance variables

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "environment" {
+	description = "Name of the environment"
+	type = string
+	default = "development"
+}
+
+variable "instance_ami" {
+	description = "AMI to be used in EC2 instances"
+	type = string
+	default = ami-0c9db8d36d76d38ed
+}
+
+variable "instance_type" {
+	description = "Instace type to be used in EC2 instances"
+	type = "string"
+	default = "t3.micro"
+}
+
+variable "instance_name" {
+	description = "Value of the Name Tag for the EC2 instance"
+	type = string
+	default = "task3-instance
+}
+
